da34ddb4532aa6c076e70af8771e2b50c2e45563
forhad khan <forhadkhan2011@gmail.com>
2023-06-02 01:31:49 +0600

    setup done

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Application, NextFunction, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response, next: NextFunction) =>
+  res.send('Hello World!')
+)
+export default app
