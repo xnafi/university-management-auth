35f0a492c94e5f7e38b876b365c21a8ae8298320
forhad khan <forhadkhan2011@gmail.com>
2023-06-04 21:36:58 +0600

    hello

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, NextFunction, Request, Response } from 'express'
 import cors from 'cors'
 const app: Application = express()
+const port = 5000
 
 app.use(cors())
 app.use(express.json())
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,16 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
-
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function main() {
   try {
-    await mongoose.connect(config.database_url);
-    console.log("Successfully connected");
+    await mongoose.connect(config.database_url as string)
+    console.log('Successfully connected')
     app.listen(config.port, () =>
       console.log(`app app listening on ${config.port}!`)
-    );
+    )
   } catch (error) {
-    console.log(`error occurred`, error);
+    console.log(`error occurred`, error)
   }
-
 }
-main();
+main()
