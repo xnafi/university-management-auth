04d9239a3ed5536370cd2e501c6d693a9aad0906
forhad khan <forhadkhan2011@gmail.com>
2023-06-04 21:42:31 +0600

    hello

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,9 @@
-import express, { Application, NextFunction, Request, Response } from 'express'
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 const app: Application = express()
-const port = 5000
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req: Request, res: Response, next: NextFunction) =>
-  res.send('Hello World!')
-)
+app.get('/', (req: Request, res: Response) => res.send('Hello World!'))
 export default app
